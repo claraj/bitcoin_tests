e5948e49cb40d6734664f5755dfe1dbf03a9134b
clara <10088152+claraj@users.noreply.github.com>
2020-03-03 10:47:51 -0600

    note on return_value vs side_effect

--- test_bitcoin.py
+++ test_bitcoin.py
@@ -13,6 +13,8 @@ class TestBitCoin(TestCase):
         # Example response. rate_float is 8735.44
         example_rate_float = 8735.44
         mock_response = {"time":{"updated":"Mar 3, 2020 16:14:00 UTC","updatedISO":"2020-03-03T16:14:00+00:00","updateduk":"Mar 3, 2020 at 16:14 GMT"},"disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org","chartName":"Bitcoin","bpi":{"USD":{"code":"USD","symbol":"&#36;","rate":"8,735.4400","description":"United States Dollar","rate_float": example_rate_float},"GBP":{"code":"GBP","symbol":"&pound;","rate":"6,815.3466","description":"British Pound Sterling","rate_float":6815.3466},"EUR":{"code":"EUR","symbol":"&euro;","rate":"7,813.8161","description":"Euro","rate_float":7813.8161}}}
+        # if it's reasonable to always expect the same return value, 
+        # you can set the return_value instead of side_effect. 
         mock_api_call.return_value = mock_response 
         example_dollars = 99.99
         expected_conversion = example_rate_float * example_dollars
